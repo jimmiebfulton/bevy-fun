c0fb804c12d550493d5d5f2eb3cc5a6c4f4af128
Jimmie Fulton <jimmie.fulton@gmail.com>
2021-05-01 22:20:37 -0700

    Add ability to close application using ESC.  Change background color

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,11 @@ mod systems;
 
 fn main() {
     App::build()
+        .insert_resource(ClearColor(Color::rgb(0.09, 0.09, 0.09)))
         .add_plugins(DefaultPlugins)
         .add_plugin(InputsPlugin)
-        // .add_plugin(HelloPlugin)
+        .add_plugin(HelloPlugin)
         .add_startup_system(systems::init::init.system())
+        .add_system(bevy::input::system::exit_on_esc_system.system())
         .run();
 }
